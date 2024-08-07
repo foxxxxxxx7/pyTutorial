260a3ffea3511d4e30d74f3d50d664551d2dd905
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-07 17:30:07 +0100

    Run python file with command prompt

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-import smtplib
+print("Hello World!")
 
-sender= "pytut45@gmail.com"
-receiver= "rob.fox7@hotmail.com"
-password= "Passw0rd123!"
-subject= "Email Subject"
-body= "Email Body"
+name=input("What's your name?: ")
 
-message=f"""From: Sender Fox {sender}
-To: Reciever Fox {receiver}
-Subject: {subject}\n
-{body}
-"""
-
-server=smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-
-try:
-    server.login(sender, password)
-    print("Logged in...")
-    server.sendmail(sender, receiver, message)
-    print("Email has been sent!")
-
-except smtplib.SMTPAuthenticationError:
-    print("Unable to sign in!")
+print("Hello"+name)
