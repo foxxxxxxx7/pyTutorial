8255b1020b4e60fd32ab473785c251d42a1a022f
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-10 16:28:46 +0100

    Add Tic-Tac-Toe game

--- main.py
+++ main.py
@@ -1,117 +1,122 @@
-import os
 from tkinter import *
-from tkinter import filedialog, colorchooser, font, messagebox
+import random
+def next_turn(row, column):
+
+    global player
+
+    if buttons[row][column]["text"] == "" and check_winner() is False:
+
+        if player == players[0]:
+
+            buttons[row][column]["text"] = player
+
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=(players[1]+" turn"))
+
+            elif check_winner() is True:
+                label.config(text=(players[0] + " wins"))
+
+            elif check_winner() == "Tie":
+                label.config(text=("Tie"))
+
+        else:
+
+            buttons[row][column]["text"] = player
+
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=(players[0]+" turn"))
+
+            elif check_winner() is True:
+                label.config(text=(players[1] + " wins"))
+
+            elif check_winner() == "Tie":
+                label.config(text=("Tie"))
 
 
-def change_colour():
-    colour = colorchooser.askcolor(title="Choose your colour")[1]
-    if colour:
-        text_area.config(fg=colour)
+def check_winner():
 
+    for row in range(3):
+        if buttons[row][0]["text"] == buttons[row][1]["text"] == buttons[row][2]["text"] != "":
+            buttons[row][0].config(bg="green")
+            buttons[row][1].config(bg="green")
+            buttons[row][2].config(bg="green")
+            return True
 
-def change_font(*args):
-    text_area.config(font=(font_name.get(), font_size.get()))
+    for column in range(3):
+        if buttons[0][column]["text"] == buttons[1][column]["text"] == buttons[2][column]["text"] != "":
+            buttons[0][column].config(bg="green")
+            buttons[1][column].config(bg="green")
+            buttons[2][column].config(bg="green")
+            return True
 
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        buttons[0][0].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][2].config(bg="green")
+        return True
 
-def new_file():
-    window.title("Untitled")
-    text_area.delete(1.0, END)
+    elif buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        buttons[0][2].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][0].config(bg="green")
+        return True
 
+    elif empty_spaces() is False:
+        for row in range(3):
+            for column in range(3):
+                buttons[row][column].config(bg="yellow")
+        return "Tie"
 
-def open_file():
-    file_path = filedialog.askopenfilename(defaultextension=".txt",
-                                           filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
-    if file_path:
-        with open(file_path, "r") as file:
-            window.title(os.path.basename(file_path))
-            text_area.delete(1.0, END)
-            text_area.insert(1.0, file.read())
+    else:
+        return False
+def empty_spaces():
 
+    spaces = 9
 
-def save_file():
-    file_path = filedialog.asksaveasfilename(initialfile="untitled.txt", defaultextension=".txt",
-                                             filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
-    if file_path:
-        with open(file_path, "w") as file:
-            window.title(os.path.basename(file_path))
-            file.write(text_area.get(1.0, END))
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]["text"] != "":
+                spaces -= 1
 
+    if spaces == 0:
+        return False
+    else:
+        return True
 
-def cut():
-    text_area.event_generate("<<Cut>>")
+def new_game():
+    global player
 
+    player = random.choice(players)
 
-def copy():
-    text_area.event_generate("<<Copy>>")
+    label.config(text=player+ " turn")
 
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="", bg="#F0F0F0")
 
-def paste():
-    text_area.event_generate("<<Paste>>")
-
-
-def about():
-    messagebox.showinfo("About this program", "This is a simple text editor")
-
-
-def quit():
-    window.destroy()
-
-
-# Initialize the main window
 window = Tk()
-window.title("Text Editor")
+window.title("Tic-Tac-Toe")
+players = ["x","o"]
+player = random.choice(players)
+buttons = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]]
 
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 500
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-x = int((screen_width / 2) - (WINDOW_WIDTH / 2))
-y = int((screen_height / 2) - (WINDOW_HEIGHT / 2))
-window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{x}+{y}")
+label = Label(text=player + " turn", font=("consolas", 40))
+label.pack(side="top")
 
-font_name = StringVar(value="Arial")
-font_size = StringVar(value="25")
+reset_button = Button(text= "Reset", font=("consolas", 20), command=new_game)
+reset_button.pack(side="top")
 
-# Text area setup
-text_area = Text(window, font=(font_name.get(), font_size.get()))
-text_area.grid(sticky=N + E + S + W)
-
-# Scrollbar setup
-scroll_bar = Scrollbar(text_area, command=text_area.yview)
-text_area.config(yscrollcommand=scroll_bar.set)
-scroll_bar.pack(side=RIGHT, fill=Y)
-
-# Toolbar setup
 frame = Frame(window)
-frame.grid()
+frame.pack()
 
-Button(frame, text="Color", command=change_colour).grid(row=0, column=0)
-OptionMenu(frame, font_name, *font.families(), command=change_font).grid(row=0, column=1)
-Spinbox(frame, from_=1, to=100, textvariable=font_size, command=change_font).grid(row=0, column=2)
-
-# Menu setup
-menu_bar = Menu(window)
-window.config(menu=menu_bar)
-
-file_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New", command=new_file)
-file_menu.add_command(label="Open", command=open_file)
-file_menu.add_command(label="Save", command=save_file)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=quit)
-
-edit_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Edit", menu=edit_menu)
-edit_menu.add_command(label="Cut", command=cut)
-edit_menu.add_command(label="Copy", command=copy)
-edit_menu.add_command(label="Paste", command=paste)
-
-help_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Help", menu=help_menu)
-help_menu.add_command(label="About", command=about)
-
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(frame, text="",font=('consolas',40), width=5, height=2,
+                                      command=lambda row = row, column = column: next_turn(row,column))
+        buttons[row][column].grid(row=row,column=column)
 
 window.mainloop()
