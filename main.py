d197f03268749164cc3c374714bda5cf28c8d9c0
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-15 23:02:30 +0100

    Add simple weight converter

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-weight = float(input("Enter weight: "))
-unit = input("KG or lbs? (K or L): ").strip().lower()
+unit = input("Is this temperature in Celsius or Fahrenheit (C or F): ").strip().lower()
+temp = float (input("Enter the temperature: "))
 
-if unit == "k":
-    converted_weight = weight * 2.205
-    unit = "lbs"
-elif unit == "l":
-    converted_weight = weight / 2.205
-    unit = "kgs"
+if unit == "c":
+    temp = round((temp * 9) / 5 + 32, 1)
+    print(f"The temperature in Fahrenheit is: {temp} °F")
+elif unit == "f":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp} °C")
 else:
-    print(f"{unit.upper()} was not valid")
-    exit()
-
-print(f"Your weight is: {round(converted_weight, 1)} {unit.capitalize()}.")
+    print(f"{unit.upper()} is an invalid unit of measurement")
