26d2b44b04e5002f42ff68d46a8d20f24dd8ae8f
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-16 22:42:44 +0100

    Add compound interest calculator

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-email = input("Enter your email: ")
+principle = 0
+rate = 0
+time = 0
 
-index = email.index("@") # remove this and follow comments below for less lines of code
+while principle <= 0:
+    principle = float(input("Enter the principle amount: "))
+    if principle <=0:
+        print("Principle cannot be less than or equal to zero")
 
-username = email[:index] #domain = email[email.index("@") + 1:]
-domain = email[index + 1:]  #username = email[:email.index("@")]
+while rate <= 0:
+    rate = float(input("Enter the interest rate: "))
+    if rate <=0:
+        print("Interest rate cannot be less than or equal to zero")
 
-print(f"your username id {username} and domain is {domain}")
+while time <= 0:
+    time = int(input("Enter the time in years: "))
+    if time <=0:
+        print("Time cannot be less than or equal to zero")
+
+total = principle * pow((1 + rate /100), time)
+print(f"Balance after {time} years: €{total:.2f}")
