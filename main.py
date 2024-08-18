ad0ed564b3f9bade6466698642c8c1ec753fc96b
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-18 22:20:05 +0100

    Add concession stand

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-basket = []
+menu = {"pizza": 5.00,
+        "popcorn": 3.00,
+        "chocolate": 2.00,
+        "sweets": 1.50,
+        "soda": 2.00,
+        "water": 1.00,
+        "nachos": 3.50,
+        "chips": 4.00}
+
+cart = []
 total = 0
 
-while True:
-    food = input("Enter the food you want to buy (q to Quit): ").strip()
-    if food.lower() == "q":
-        break
-    try:
-        price = float(input(f"Enter the price of {food}: €").strip())
-        basket.append((food, price))
-        total += price
-    except ValueError:
-        print("Please enter a valid price.")
+print("------------- MENU ---------------")
+for key, value in menu.items():
+    print(f"{key:10}: €{value:.2f}")
+print("----------------------------------")
 
-print("\n------ YOUR BASKET ------")
-print(", ".join(food for food, _ in basket))
-print(f"Your total is: €{total:.2f}")
+while True:
+    food = input("Select an item(q to Quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+print("------------- ORDER ---------------")
+for food in cart:
+       total += menu.get(food)
+       print(food, end=" ")
+
+print()
+print(f"Total is: €{total:.2f}")
+
+print("----------------------------------")
