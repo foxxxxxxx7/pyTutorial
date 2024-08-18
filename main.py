ef08d5d3b44b541280af1c4e486844d561c993af
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-18 22:22:52 +0100

    Optimize concession stand

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-menu = {"pizza": 5.00,
-        "popcorn": 3.00,
-        "chocolate": 2.00,
-        "sweets": 1.50,
-        "soda": 2.00,
-        "water": 1.00,
-        "nachos": 3.50,
-        "chips": 4.00}
+menu = {
+    "pizza": 5.00,
+    "popcorn": 3.00,
+    "chocolate": 2.00,
+    "sweets": 1.50,
+    "soda": 2.00,
+    "water": 1.00,
+    "nachos": 3.50,
+    "chips": 4.00
+}
 
-cart = []
-total = 0
+def print_menu(menu):
+    print("------------- MENU ---------------")
+    for item, price in menu.items():
+        print(f"{item:10}: €{price:.2f}")
+    print("----------------------------------")
 
-print("------------- MENU ---------------")
-for key, value in menu.items():
-    print(f"{key:10}: €{value:.2f}")
-print("----------------------------------")
+def take_order(menu):
+    cart = []
+    while (food := input("Select an item(q to Quit): ").lower()) != "q":
+        if food in menu:
+            cart.append(food)
+        else:
+            print("Item not on the menu. Please try again.")
+    return cart
 
-while True:
-    food = input("Select an item(q to Quit): ").lower()
-    if food == "q":
-        break
-    elif menu.get(food) is not None:
-        cart.append(food)
+def print_order(cart, menu):
+    total = sum(menu[item] for item in cart)
+    print("------------- ORDER ---------------")
+    print(" ".join(cart))
+    print(f"Total is: €{total:.2f}")
+    print("----------------------------------")
 
-print("------------- ORDER ---------------")
-for food in cart:
-       total += menu.get(food)
-       print(food, end=" ")
+def main():
+    print_menu(menu)
+    cart = take_order(menu)
+    print_order(cart, menu)
 
-print()
-print(f"Total is: €{total:.2f}")
-
-print("----------------------------------")
+if __name__ == "__main__":
+    main()
