021ec6c0cb7a7c1555feef1129badf9fdbf108eb
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-19 20:55:02 +0100

    Optimize guessing game

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import random
 
-lowest_number = 1
-highest_number = 100
+lowest_number, highest_number = 1, 100
 answer = random.randint(lowest_number, highest_number)
 guesses = 0
-is_running = True
 
 print("Python Number Guessing Game")
 print(f"Select a number between {lowest_number} and {highest_number}")
 
-while is_running:
+while True:
     guess = input("Enter your guess: ")
 
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
+    if not guess.isdigit():
+        print("Invalid guess: Not a number")
+        continue
 
-        if guess < lowest_number or guess > highest_number:
-            print("Invalid guess: out of range")
-            print(f"Select a number between {lowest_number} and {highest_number}")
-        elif guess < answer:
-            print("Too low, try again")
-        elif guess > answer:
-            print("Too high, try again")
-        else:
-            print(f"CORRECT! {answer} is correct")
-            print(f"It took {guesses} guesses")
-            is_running = False
+    guess = int(guess)
+    guesses += 1
 
+    if not (lowest_number <= guess <= highest_number):
+        print(f"Invalid guess: out of range. Select a number between {lowest_number} and {highest_number}")
+    elif guess < answer:
+        print("Too low, try again")
+    elif guess > answer:
+        print("Too high, try again")
     else:
-        print("Invalid guess: NaN")
-        print(f"Select a number between {lowest_number} and {highest_number}")
+        print(f"CORRECT! {answer} is correct. It took {guesses} guesses")
+        break
