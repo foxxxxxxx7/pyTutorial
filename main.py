f21a3b7663d4513a47f0fa6aca0dd463a9345d6f
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-21 22:08:24 +0100

    Add encryption programme

--- main.py
+++ main.py
@@ -1,42 +1,30 @@
 import random
+import string
 
-dice_art = {
-    1: ("┌─────────┐",
-        "│         │",
-        "│    ●    │",
-        "│         │",
-        "└─────────┘"),
-    2: ("┌─────────┐",
-        "│  ●      │",
-        "│         │",
-        "│      ●  │",
-        "└─────────┘"),
-    3: ("┌─────────┐",
-        "│  ●      │",
-        "│    ●    │",
-        "│      ●  │",
-        "└─────────┘"),
-    4: ("┌─────────┐",
-        "│  ●   ●  │",
-        "│         │",
-        "│  ●   ●  │",
-        "└─────────┘"),
-    5: ("┌─────────┐",
-        "│  ●   ●  │",
-        "│    ●    │",
-        "│  ●   ●  │",
-        "└─────────┘"),
-    6: ("┌─────────┐",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "└─────────┘")
-}
+chars = string.punctuation + string.digits + string.ascii_letters + " "
+chars = list(chars)
+key = chars.copy()
 
-number_of_dice = int(input("How many dice?: "))
-dice = [random.randint(1, 6) for _ in range(number_of_dice)]
+random.shuffle(key)
 
-for line in range(5):
-    print("".join(dice_art[die][line] for die in dice))
+#ENCRYPT
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
 
-print(f"Total: {sum(dice)}")
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message: {plain_text}")
+print(f"encrypted message: {cipher_text}")
+
+#DECRYPT
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"decrypted message: {plain_text}")
