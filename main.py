a73a51ca6fc4f1703a37612b0ffe6d465bc9930d
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-28 20:39:10 +0100

    Optimize resistoe calculator

--- main.py
+++ main.py
@@ -1,54 +1,27 @@
-RESISTOR_VALUES = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7,
-    'grey': 8, 'white': 9}
-TOLERANCE_VALUES = {'grey': 0.05, 'violet': 0.1, 'blue': 0.25, 'green': 0.5, 'brown': 1, 'red': 2, 'gold': 5,
-    'silver': 10, }
-
+RESISTOR_VALUES = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
+TOLERANCE_VALUES = {'grey': 0.05, 'violet': 0.1, 'blue': 0.25, 'green': 0.5, 'brown': 1, 'red': 2, 'gold': 5, 'silver': 10}
 
 def resistor_label(colors):
-    value = ""
-    exponent = 0
-    tolerance = 0
-    unit = " ohms"
     if len(colors) == 4:
-        for i, color in enumerate(colors):
-            if i < 2:
-                value += str(RESISTOR_VALUES[color])
-            elif i == 2:
-                exponent = RESISTOR_VALUES[color]
-            elif i == 3:
-                tolerance = TOLERANCE_VALUES[color]
+        digits, exponent_color, tolerance_color = colors[:2], colors[2], colors[3]
     else:
-        for i, color in enumerate(colors):
-            if i < 3:
-                value += str(RESISTOR_VALUES[color])
-            elif i == 3:
-                exponent = RESISTOR_VALUES[color]
-            elif i == 4:
-                tolerance = TOLERANCE_VALUES[color]
+        digits, exponent_color, tolerance_color = colors[:3], colors[3], colors[4]
 
-    final_value = (float(value) * (10 ** exponent))
-    if len(colors) == 4:
-        if 2 <= exponent <= 5:
-            final_value = final_value / 1000
-            unit = " kiloohms"
-        elif 6 <= exponent <= 8:
-            final_value = final_value / 1000 ** 2
-            unit = " megaohms"
-        elif 9 <= exponent <= 11:
-            final_value = final_value / 1000 ** 3
-            unit = " gigaohms"
-    else:
-        if 1 <= exponent <= 3:
-            final_value = final_value / 1000
-            unit = " kiloohms"
-        elif 4 <= exponent <= 6:
-            final_value = final_value / 1000 ** 2
-            unit = " megaohms"
-        elif 7 <= exponent <= 11:
-            final_value = final_value / 1000 ** 3
-            unit = " gigaohms"
-    if final_value == 0:
-        return f'{int(final_value)}{unit}'
-    elif str(final_value)[-2:] == '.0':
-        return f"{int(final_value)}{unit} ±{tolerance}%"
+    value = int(''.join(str(RESISTOR_VALUES[color]) for color in digits))
+    exponent = RESISTOR_VALUES[exponent_color]
+    tolerance = TOLERANCE_VALUES[tolerance_color]
+
+    final_value = value * (10 ** exponent)
+    unit = " ohms"
+    if exponent >= 9:
+        final_value /= 10 ** 9
+        unit = " gigaohms"
+    elif exponent >= 6:
+        final_value /= 10 ** 6
+        unit = " megaohms"
+    elif exponent >= 3:
+        final_value /= 10 ** 3
+        unit = " kiloohms"
+
+    final_value = int(final_value) if final_value.is_integer() else final_value
     return f"{final_value}{unit} ±{tolerance}%"
