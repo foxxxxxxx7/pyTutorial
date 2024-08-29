1a5272804d02514ce36ee38a56d3a6e4813cb1f7
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-29 21:45:15 +0100

    Add rotational cipher

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-RESISTOR_VALUES = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
-TOLERANCE_VALUES = {'grey': 0.05, 'violet': 0.1, 'blue': 0.25, 'green': 0.5, 'brown': 1, 'red': 2, 'gold': 5, 'silver': 10}
+def rotate(text, key):
+    cipher = []
+    letters_list = list('abcdefghijklmnopqrstuvwxyz')
 
-def resistor_label(colors):
-    if len(colors) == 4:
-        digits, exponent_color, tolerance_color = colors[:2], colors[2], colors[3]
-    else:
-        digits, exponent_color, tolerance_color = colors[:3], colors[3], colors[4]
+    def get_final_index(letter, key):
+        letter_index = letters_list.index(letter)
+        final_index = letter_index + key
+        if final_index > 25:
+            final_index -= 26
+        return final_index
 
-    value = int(''.join(str(RESISTOR_VALUES[color]) for color in digits))
-    exponent = RESISTOR_VALUES[exponent_color]
-    tolerance = TOLERANCE_VALUES[tolerance_color]
+    for c in text:
+        if c.isupper():
+            final_index = get_final_index(c.lower(), key)
+            cipher.append(letters_list[final_index].upper())
+        elif c not in letters_list:
+            cipher.append(c)
+        else:
+            final_index = get_final_index(c, key)
+            cipher.append(letters_list[final_index])
 
-    final_value = value * (10 ** exponent)
-    unit = " ohms"
-    if exponent >= 9:
-        final_value /= 10 ** 9
-        unit = " gigaohms"
-    elif exponent >= 6:
-        final_value /= 10 ** 6
-        unit = " megaohms"
-    elif exponent >= 3:
-        final_value /= 10 ** 3
-        unit = " kiloohms"
-
-    final_value = int(final_value) if final_value.is_integer() else final_value
-    return f"{final_value}{unit} ±{tolerance}%"
+    return ''.join(cipher)
