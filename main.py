3ba7b33d7356c4979549be05cd447ac3b44f2997
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-08-31 10:23:17 +0100

    Add ISBN 10 digit verifier

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-import operator
+def is_valid(isbn):
+    """
+    Validates a 10-digit ISBN number.
 
+    This function checks if the given ISBN string is a valid 10-digit ISBN. It considers
+    ISBN numbers with or without hyphens. The validation follows the ISBN-10 checksum formula:
+    - The first 9 characters should be digits.
+    - The last character can be a digit or the letter 'X', which represents the value 10.
+    - The checksum is calculated by multiplying each of the first 9 digits by a decreasing
+      weight from 10 to 2, and adding the last character (or its value if it's 'X') to the result.
+    - The total sum, including the last character, must be divisible by 11 for the ISBN to be valid.
 
-def answer(question):
-    ops = {'plus': operator.add, 'minus': operator.sub, 'multiplied': operator.mul, 'divided': operator.truediv}
-    question = question.removeprefix('What is').strip('?').split()
-    if not question or (question[-1] in ops and question[-2].isdigit()) or (
-            question[0] in ops and question[1].isdigit()):
-        raise ValueError("syntax error")
+    Args:
+        isbn (str): The ISBN number to be validated, which may include hyphens.
 
-    numbers = []
-    operations = []
+    Returns:
+        bool: True if the ISBN is valid, False otherwise.
 
-    for word in question:
-        if word.isdigit() or (word.startswith('-') and word[1:].isdigit()):
-            numbers.append(int(word))
-        elif word in ops:
-            operations.append(ops[word])
-        elif word == "by":
-            continue
-        else:
-            raise ValueError("unknown operation")
-    if len(numbers) != len(operations) + 1:
-        raise ValueError("syntax error")
-    result = numbers[0]
-    for i, operation in enumerate(operations):
-        result = operation(result, numbers[i + 1])
+    """
+    iteration = 10
+    result = 0
+    isbn = list(isbn.replace("-", ""))
+    if len(isbn) != 10:
+        return False
 
-    return result
+    final_char = isbn.pop()
+    for character in isbn:
+        if character.isalpha():
+            return False
+
+    if final_char == 'X':
+        final_char = 10
+    elif not final_char.isdigit():
+        return False
+
+    for character in isbn:
+        result += int(character) * iteration
+        iteration -= 1
+    return (result + int(final_char)) % 11 == 0
