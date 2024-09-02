3e4f23a14ab7ae3ffabfd3801e4ecd638ca79f57
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-02 22:10:38 +0100

    Add perfect numbers

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-def generate_step(number):
-    if number % 2 == 0:
-        yield number / 2
-    else:
-        yield number * 3 + 1
+def classify(number):
+    """ A perfect number equals the sum of its positive divisors.
 
-
-def steps(number):
+    :param number: int a positive integer
+    :return: str the classification of the input integer
+    """
+    factors = []
     if number <= 0:
-        raise ValueError("Only positive integers are allowed")
+        raise ValueError("Classification is only possible for positive integers.")
 
-    count = 0
-    while number != 1:
-        step = generate_step(number)
-        number = next(step)
-        count += 1
-    return count
+    for n in range(1, number):
+        if number % n == 0:
+            factors.append(n)
+    if sum(factors) == number:
+        return 'perfect'
+    if sum(factors) > number:
+        return 'abundant'
+    if sum(factors) < number:
+        return 'deficient' .
