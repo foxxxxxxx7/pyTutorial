651890a98eb2cc1d5aa3e6e9f28dee0a74382b70
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-04 21:42:00 +0100

    Add Rock, Paper, Scissors

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-RESISTOR_VALUES = {
-        'black': 0,
-        'brown': 1,
-        'red': 2,
-        'orange': 3,
-        'yellow': 4,
-        'green': 5,
-        'blue': 6,
-        'violet': 7,
-        'grey': 8,
-        'white': 9
-    }
-def value(colors):
-    value = ""
-    for i, color in enumerate(colors):
-        if i < 2:
-            value += str(RESISTOR_VALUES[color])
-    return int(value)
+import random
+
+options = ("rock", "paper", "scissors")
+running = True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+
+    if not input("Play again? (y/n): ").lower() == "y":
+        running = False
+
+print("Thanks for playing!")
