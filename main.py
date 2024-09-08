861ecb2a76ee820d6d5602d218479caec69e47d8
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-08 22:13:50 +0100

    Add Solar Calculator

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-def find(search_list, value):
-    start = 0
-    end = len(search_list) - 1
-    while start <= end:
-        mid_index = (start + end) // 2
-        if search_list[mid_index] == value:
-            return mid_index
-        elif search_list[mid_index] < value:
-            start = mid_index + 1
-        else:
-            end = mid_index - 1
-    raise ValueError('value not in array')
+class SpaceAge:
+    def __init__(self, seconds):
+        self.seconds = seconds
+        self.earth_year = 31557600
+
+    def on_mercury(self):
+        return round((self.seconds / self.earth_year / 0.2408467), 2)
+
+    def on_venus(self):
+        return round((self.seconds / self.earth_year / 0.61519726), 2)
+
+    def on_earth(self):
+        return round((self.seconds / self.earth_year), 2)
+
+    def on_mars(self):
+        return round((self.seconds / self.earth_year / 1.8808158), 2)
+
+    def on_jupiter(self):
+        return round((self.seconds / self.earth_year / 11.862615), 2)
+
+    def on_saturn(self):
+        return round((self.seconds / self.earth_year / 29.447498), 2)
+
+    def on_uranus(self):
+        return round((self.seconds / self.earth_year / 84.016846), 2)
+
+    def on_neptune(self):
+        return round((self.seconds / self.earth_year / 164.79132), 2)
