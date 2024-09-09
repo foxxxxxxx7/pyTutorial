e6b5c30d95ee12427d1f1557cef0025c464993fa
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-09 21:26:38 +0100

    Pig Latin translator

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-class SpaceAge:
-    def __init__(self, seconds):
-        self.seconds = seconds
-        self.earth_year = 31557600
-
-    def on_mercury(self):
-        return round((self.seconds / self.earth_year / 0.2408467), 2)
-
-    def on_venus(self):
-        return round((self.seconds / self.earth_year / 0.61519726), 2)
-
-    def on_earth(self):
-        return round((self.seconds / self.earth_year), 2)
-
-    def on_mars(self):
-        return round((self.seconds / self.earth_year / 1.8808158), 2)
-
-    def on_jupiter(self):
-        return round((self.seconds / self.earth_year / 11.862615), 2)
-
-    def on_saturn(self):
-        return round((self.seconds / self.earth_year / 29.447498), 2)
-
-    def on_uranus(self):
-        return round((self.seconds / self.earth_year / 84.016846), 2)
-
-    def on_neptune(self):
-        return round((self.seconds / self.earth_year / 164.79132), 2)
+VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
+def translate(text):
+    words = text.split()
+    result = []
+    for w in words:
+        if w[:2] == 'yt':
+            result.append(w + 'ay')
+        elif w[0] == 'y':
+            result.append(w[1:] + 'yay')
+        elif w[:2] == 'xr' or w[0] in VOWELS:
+            result.append(w + 'ay')
+        elif 'qu' in w:
+            index_u = w.find('qu') + 2
+            result.append(w[index_u:] + w[:index_u] + 'ay')
+        elif w[0] not in VOWELS:
+            for i, l in enumerate(w):
+                if l in VOWELS:
+                    result.append(w[i:] + w[:i] + 'ay')
+                    break
+    return ' '.join(result)
