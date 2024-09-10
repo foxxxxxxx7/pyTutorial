6dd623e8239b68bcb6a9c713817cf35f6699789a
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-10 21:52:44 +0100

    Add Bracket Matcher

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-VOWELS = ['a', 'e', 'i', 'o', 'u', 'y']
-def translate(text):
-    words = text.split()
-    result = []
-    for w in words:
-        if w[:2] == 'yt':
-            result.append(w + 'ay')
-        elif w[0] == 'y':
-            result.append(w[1:] + 'yay')
-        elif w[:2] == 'xr' or w[0] in VOWELS:
-            result.append(w + 'ay')
-        elif 'qu' in w:
-            index_u = w.find('qu') + 2
-            result.append(w[index_u:] + w[:index_u] + 'ay')
-        elif w[0] not in VOWELS:
-            for i, l in enumerate(w):
-                if l in VOWELS:
-                    result.append(w[i:] + w[:i] + 'ay')
-                    break
-    return ' '.join(result)
+def is_paired(input_string):
+    bracket_pairs = {"(": ")", "{": "}", "[": "]"}
+    stack = []
+
+    for c in input_string:
+        if c in bracket_pairs.keys():
+            stack.append(c)
+        elif c in bracket_pairs.values():
+            if len(stack) == 0:
+                return False
+            last_open = stack.pop()
+            if bracket_pairs[last_open] != c:
+                return False
+
+    return len(stack) == 0
