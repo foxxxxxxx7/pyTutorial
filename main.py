84c9d7874887ac21a7346266e0805a518523ce7e
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-11 22:12:28 +0100

    Add Diamond

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-PLAIN = 'abcdefghijklmnopqrstuvwxyz'
-DIGITS = '1234567890'
-
-
-def encode(plain_text):
-    cipher = ''
-    for c in plain_text.lower():
-        if c in PLAIN:
-            index = PLAIN.index(c) + 1
-            cipher += PLAIN[-index]
-        elif c in DIGITS:
-            cipher += c
-        continue
-
-    return ' '.join(cipher[i:i + 5] for i in range(0, len(cipher), 5))
-
-
-def decode(ciphered_text):
-    plain = ciphered_text.replace(' ', '')
-    result = ''
-    for c in plain:
-        if c in PLAIN:
-            index = PLAIN.index(c) + 1
-            result += PLAIN[-index]
-        if c in DIGITS:
-            result += c
-    return result
+LETTERS =  'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def rows(letter):
+    result = []
+    l_index = LETTERS.index(letter)
+    for i in range(l_index + 1):
+        if i == 0:  
+            result.append(' ' * l_index + LETTERS[i] + ' ' * l_index)
+        else:  
+            inner_spaces = 2 * i - 1
+            result.append(' ' * (l_index - i) + LETTERS[i] + ' ' * inner_spaces + LETTERS[i] + ' ' * (l_index - i))
+    return result + result[-2::-1]
