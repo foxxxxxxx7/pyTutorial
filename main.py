ee9f9a2576c4c803b5c874604f7ebafe183b69f8
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-18 21:53:57 +0100

    Add Luhn Validator as a Class

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-def count_words(sentence):
-    result = {}
-    sentence = sentence.lower().translate(str.maketrans(",:._!&@$%^", "          ")).strip("'")
-    words = sentence.lower().split()
-    for word in words:
-        word = word.strip("'")
-        if word in result:
-            result[word] += 1
-        else:
-            result[word] = 1
-    return result
+class Luhn:
+    def __init__(self, card_num):
+        self.card_num = card_num
+
+    def valid(self):
+        odd_total = 0
+        even_total = 0
+        total = 0
+
+        clean_num = self.card_num[::-1].replace(' ', '')
+        if not clean_num.isdigit():
+            return False
+
+        for n in clean_num[::2]:
+            odd_total += int(n)
+        for n in clean_num[1::2]:
+            n = int(n) * 2
+            if n > 9:
+                n -= 9
+            even_total += n
+        total = even_total + odd_total
+        return total % 10 == 0 and len(clean_num) > 1
