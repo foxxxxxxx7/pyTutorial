767454d53cb2a2a729452c49df380e5948cbf697
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-18 21:52:42 +0100

    Add Word Counter

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-NUMERALS = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I"
-}
-def roman(number):
-    result = ''
-    for key in NUMERALS:
-        while number >= key:
-            result += NUMERALS[key]  
-            number -= key  
+def count_words(sentence):
+    result = {}
+    sentence = sentence.lower().translate(str.maketrans(",:._!&@$%^", "          ")).strip("'")
+    words = sentence.lower().split()
+    for word in words:
+        word = word.strip("'")
+        if word in result:
+            result[word] += 1
+        else:
+            result[word] = 1
     return result
