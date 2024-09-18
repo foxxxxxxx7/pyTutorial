80fd5d062e42fe6b1227a80f359e30bfe751abc8
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-18 21:51:33 +0100

    Add Roman Numeral Converter

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
-def decode(string):
+NUMERALS = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
+def roman(number):
     result = ''
-    multiply = 0
-    for s in string:
-        if s.isdigit():
-            multiply = (multiply * 10) + int(s)
-        else:
-            result += max(1, multiply) * s
-            multiply = 0
-    return result
-def encode(string):
-    prev_s = ''
-    count = 1
-    result = ''
-    for s in string:
-        if s == prev_s or s == '':
-            count += 1
-            prev_s = s
-        elif s != prev_s:
-            if count != 1:
-                result += str(count) + prev_s
-                count = 1
-                prev_s = s
-            else:
-                result += prev_s
-                count = 1
-                prev_s = s
-    if count != 1:
-        result += str(count) + prev_s
-    else:
-        result += prev_s
+    for key in NUMERALS:
+        while number >= key:
+            result += NUMERALS[key]  
+            number -= key  
     return result
