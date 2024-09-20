ecb766971fa90ebd686e1138c5c1a10b4284e6c8
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-20 22:47:41 +0100

    Add DnD Charcter Creator

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-def slices(series, length):
-    result = []
-    if length == 0:
-        raise ValueError("slice length cannot be zero")
-    if length < 0:
-        raise ValueError("slice length cannot be negative")
-    if len(series) == 0:
-        raise ValueError("series cannot be empty")
-    if length > len(series):
-        raise ValueError("slice length cannot be greater than series length")
-
-    for i in range(0, len(series)):
-        if i > len(series) - length:
-            continue
-        result.append(series[i:i + length])
-    return result
+from random import randint
+def modifier(constitution_stat):
+    return (constitution_stat - 10)//2
+class Character:
+    def __init__(self):
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+    def ability(self):
+        dices = [randint(1, 6) for i in range(4)]
+        return sum(dices) - min(dices)
