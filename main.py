46b30c8b4d35fd9c7613b3e1ae612fc9ad75bf84
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-21 22:17:37 +0100

    Add Robot Name

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import random, string
 from random import randint
-def modifier(constitution_stat):
-    return (constitution_stat - 10)//2
-class Character:
+letters = string.ascii_uppercase
+class Robot:
     def __init__(self):
-        self.strength = self.ability()
-        self.dexterity = self.ability()
-        self.constitution = self.ability()
-        self.intelligence = self.ability()
-        self.wisdom = self.ability()
-        self.charisma = self.ability()
-        self.hitpoints = 10 + modifier(self.constitution)
-    def ability(self):
-        dices = [randint(1, 6) for i in range(4)]
-        return sum(dices) - min(dices)
+        self.generateName()
+    def generateName(self):
+        random.seed()
+        name = ''
+        name += random.choice(letters)
+        name += random.choice(letters)
+        name += str(randint(100,999))
+        self.name = name
+    def reset(self):
+        self.generateName()
