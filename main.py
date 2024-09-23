1b016a3b70f12d84eed410fd95c95e38734b0056
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-23 21:48:46 +0100

    Add Number cleaner

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-# Globals for the directions
-# Change the values as you see fit
-EAST = 2
-NORTH = 1
-WEST = 4
-SOUTH = 3
+class PhoneNumber:
+    def __init__(self, number):
+        self.number = self.clean_number(number)
+        self.area_code = self.number[0:3]
+        self.exchange_code = self.number[3:6]
+        self.subs_num = self.number[6:10]
 
+    def pretty(self):
+        return f"({self.area_code})-{self.exchange_code}-{self.subs_num}"
 
-class Robot:
-    def __init__(self, direction=NORTH, x_pos=0, y_pos=0):
-        self.direction = direction
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.coordinates = (x_pos, y_pos)
+    def clean_number(self, number):
+        clean_num = number.translate(str.maketrans(")(.-+", "     ")).replace(' ', '')
+        if len(clean_num) < 10:
+            raise ValueError("must not be fewer than 10 digits")
+        if len(clean_num) > 11:
+            raise ValueError("must not be greater than 11 digits")
+        if len(clean_num) == 11 and clean_num[0] != '1':
+            raise ValueError("11 digits must start with 1")
+        if (len(clean_num) == 11 and clean_num[1] == '0') or (len(clean_num) == 10 and clean_num[0] == '0'):
+            raise ValueError("area code cannot start with zero")
+        if (len(clean_num) == 11 and clean_num[1] == '1') or (len(clean_num) == 10 and clean_num[0] == '1'):
+            raise ValueError("area code cannot start with one")
+        if (len(clean_num) == 11 and clean_num[4] == '0') or (len(clean_num) == 10 and clean_num[3] == '0'):
+            raise ValueError("exchange code cannot start with zero")
+        if (len(clean_num) == 11 and clean_num[4] == '1') or (len(clean_num) == 10 and clean_num[3] == '1'):
+            raise ValueError("exchange code cannot start with one")
+        elif len(clean_num) == 11:
+            clean_num = clean_num[1:]
+        for c in clean_num:
+            if c.isalpha():
+                raise ValueError("letters not permitted")
+            elif not c.isdigit():
+                raise ValueError("punctuations not permitted")
 
-    def move(self, instructions):
-        for instruction in instructions:
-            if instruction == 'R':
-                self.direction = (self.direction % 4) + 1
-            if instruction == 'L':
-                self.direction = (self.direction -2) % 4 + 1
-            if instruction == 'A':
-                    if self.direction == NORTH:
-                        self.y_pos += 1
-                    elif self.direction == EAST:
-                        self.x_pos += 1
-                    elif self.direction == SOUTH:
-                        self.y_pos -= 1
-                    elif self.direction == WEST:
-                        self.x_pos -= 1
-
-                    self.coordinates = (self.x_pos, self.y_pos)
+        return clean_num
