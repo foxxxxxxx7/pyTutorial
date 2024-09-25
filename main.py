40a1398088ee3a1114e80a8282575276595e48b7
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-25 22:53:58 +0100

    Add queen attack checker

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from random import choice, randint
-from string import ascii_uppercase as letters
+class Queen:
+    def __init__(self, row, column):
+        if row < 0:
+            raise ValueError("row not positive")
+        if column < 0:
+            raise ValueError("column not positive")
+        if row > 7:
+            raise ValueError("row not on board")
+        if column > 7:
+            raise ValueError("column not on board")
+        self.row = row
+        self.column = column
 
-names = set()
+    def can_attack(self, another_queen):
+        if self.row == another_queen.row and self.column == another_queen.column:
+            raise ValueError("Invalid queen position: both queens in the same square")
 
-def create_name():
-    while True:
-        name = f"{choice(letters)}{choice(letters)}{str(randint(100, 999))}"
-        if name not in names:
-            names.add(name)
-            return name
-
-class Robot:
-    def __init__(self):
-        self.name = create_name()
-
-    def reset(self):
-        names.discard(self.name)
-        new_name = create_name()
-        if new_name == self.name:
-            new_name = create_name()
-        self.name = new_name
+        elif self.row == another_queen.row or self.column == another_queen.column or (
+                abs(self.row - another_queen.row) == abs(self.column - another_queen.column)):
+            return True
+        else:
+            return False
