02aedb4ac0e0fea280c09dbc8d761460b813e1bd
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-09-30 22:34:10 +0100

    Add Matrix Reader

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+class Matrix:
+    def __init__(self, matrix_string):
+        rows = matrix_string.split('\n')
+        self.matrix = []
+        for row in rows:
+            number_strings = row.split()
+            numbers = []
+            for num_str in number_strings:
+                numbers.append(int(num_str))
+            self.matrix.append(numbers)
 
+    def row(self, index):
+        return self.matrix[index - 1]
 
-def primes(limit):
-    result = []
-    for n in range(limit + 1):
-        if is_prime(n):
-            result.append(n)
-        pass
-    return result
+    def column(self, index):
+        column_elements = []
+        for row in self.matrix:
+            column_value = row[index - 1]
+            column_elements.append(column_value)
+        return column_elements
