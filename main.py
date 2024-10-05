0568718bef0469de8dd74e4190233dce78ce8166
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-10-05 22:47:26 +0100

    Optimize Acronym Translator

--- main.py
+++ main.py
@@ -1,25 +1,2 @@
-VOWELS = {'a', 'e', 'i', 'o', 'u', 'y'}
-EXCEPTIONS = {'xr', 'yt'}
-SUFFIX = 'ay'
-
-
-def translate_word(word):
-    if word[:2] in EXCEPTIONS or word[0] in VOWELS - {'y'}:
-        return f"{word}{SUFFIX}"
-
-    if word[0] == 'y':
-        return f"{word[1:]}y{SUFFIX}"
-
-    if (qu_index := word.find('qu')) != -1:
-        return f"{word[qu_index + 2:]}{word[:qu_index + 2]}{SUFFIX}"
-
-    for i, letter in enumerate(word):
-        if letter in VOWELS:
-            return f"{word[i:]}{word[:i]}{SUFFIX}"
-    return word
-
-
-def translate(text):
-    words = text.split()
-    result = (translate_word(word) for word in words)
-    return ' '.join(result)
+def abbreviate(phrase):
+    return ''.join(word[0].upper() for word in phrase.replace('-', ' ').replace('_', '').split())
