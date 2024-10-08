53b0e60605f3521e5f48606c48eb8c9e05a37c79
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-10-08 21:26:26 +0100

    Optimize Word Count

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from string import ascii_lowercase as letters
+from collections import defaultdict
 
-def is_pangram(sentence):
-    return all((letter in sentence.lower()) for letter in letters)
+
+def count_words(sentence):
+    sentence = sentence.lower().translate(str.maketrans(",:._!&@$%^", " " * 10)).strip("'")
+    words = sentence.split()
+
+    result = defaultdict(int)
+    for word in words:
+        word = word.strip("'")
+        result[word] += 1
+
+    return dict(result)
