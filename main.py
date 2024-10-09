e452420b5fd61cdecfc7083ef5982b6a12ad733b
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-10-09 21:14:09 +0100

    Add Saddle Points

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from collections import defaultdict
+def saddle_points(matrix):
+    if not matrix:
+        return []
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise ValueError("irregular matrix")
 
+    columns = {}
+    saddle_points = []
+    for row in matrix:
+        for i, value in enumerate(row):
+            if i not in columns:
+                columns[i] = []
+            columns[i].append(value)
 
-def count_words(sentence):
-    sentence = sentence.lower().translate(str.maketrans(",:._!&@$%^", " " * 10)).strip("'")
-    words = sentence.split()
-
-    result = defaultdict(int)
-    for word in words:
-        word = word.strip("'")
-        result[word] += 1
-
-    return dict(result)
+    for i, row in enumerate(matrix):
+        row_max = max(row)
+        for j, value in enumerate(row):
+            col_min = min(columns[j])
+            if value == row_max and value == col_min:
+                saddle_points.append({"row": i + 1, "column": j + 1})
+    return saddle_points
