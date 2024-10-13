df38d5155aa08b34646e9f5b04a1ad3642160457
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-10-13 20:50:44 +0100

    Optimize Tisbury Treasure Hunt

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def is_isogram(string):
-    string = ''.join(filter(str.isalpha, string)).lower()
+def get_coordinate(record):
+    return record[1]
 
-    return len(string) == len(set(string))
+
+def convert_coordinate(coordinate):
+    return tuple(coordinate)
+
+
+def compare_records(azara_record, rui_record):
+    return tuple(azara_record[1]) == rui_record[1]
+
+
+def create_record(azara_record, rui_record):
+    return azara_record + rui_record if compare_records(azara_record, rui_record) else 'not a match'
+
+
+def clean_up(combined_record_group):
+    return '\n'.join(str((item[0], item[2], item[3], item[4])) for item in combined_record_group) + '\n'
