fb5cd34a02132ea9219e60627106b2ad85db269d
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-10-15 21:27:13 +0100

    Optimize RNA Converter

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-from collections import deque
-
-
-class BufferFullException(BufferError):
-    def __init__(self, message="Circular buffer is full"):
-        super().__init__(message)
-
-
-class BufferEmptyException(BufferError):
-    def __init__(self, message="Circular buffer is empty"):
-        super().__init__(message)
-
-
-class CircularBuffer:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.buffer = deque(maxlen=capacity)
-
-    def read(self):
-        if len(self.buffer) == 0:
-            raise BufferEmptyException()
-        return self.buffer.popleft()
-
-    def write(self, data):
-        if len(self.buffer) == self.capacity:
-            raise BufferFullException()
-        self.buffer.append(data)
-
-    def overwrite(self, data):
-        if len(self.buffer) == self.capacity:
-            self.buffer.popleft()
-        self.buffer.append(data)
-
-    def clear(self):
-        self.buffer.clear()
+def to_rna(dna_strand):
+    rna_complement = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+    return ''.join(rna_complement[letter] for letter in dna_strand)
