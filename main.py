c5bf213e6ab07167b5697c25a852e966577e5cb9
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-10-16 22:57:04 +0100

    Add kindergarden

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-def to_rna(dna_strand):
-    rna_complement = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    return ''.join(rna_complement[letter] for letter in dna_strand)
+PLANTS = {"G": "Grass", "C": "Clover", "R": "Radishes", "V": "Violets"}
+
+STUDENTS = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry",]
+
+class Garden:
+    def __init__(self, diagram, students= STUDENTS):
+        self.row1, self.row2 = diagram.split("\n")
+        self.students = sorted(students)
+
+    def plants(self, student):
+        index = self.students.index(student)
+        plants = self.row1[2*index:2*index+2] + self.row2[2*index:2*index+2]
+        return [PLANTS[plant] for plant in plants]
