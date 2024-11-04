2d69237572f2375f748f475b7f835ebdd66b9dfe
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-11-04 22:41:58 +0000

    Add Factorial of a number using recursion

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-# Python program to shuffle a deck of cards
+# Factorial of a number using recursion
 
-# importing modules
-import itertools, random
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * recur_factorial(n - 1)
 
-# make a deck of cards
-deck = list(itertools.product(range(1, 15), ['Spade', 'Heart', 'Diamond', 'Club']))
 
-# shuffle the cards
-random.shuffle(deck)
+num = 7
 
-# mapping for special card values
-value_map = {
-    11: 'J',
-    12: 'Q',
-    13: 'K',
-    14: 'A'
-}
-
-# draw five cards
-print("You got:")
-for i in range(5):
-    # get the card value, check if it's in the map, otherwise use the number
-    value = value_map.get(deck[i][0], deck[i][0])
-    print(value, "of", deck[i][1])
+# check if the number is negative
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    print("The factorial of", num, "is", recur_factorial(num))
