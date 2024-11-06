e5c6924c26e2b3e38948961728c4eed902b1aa1e
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-11-06 21:24:27 +0000

    Add Transpose

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-def jpeg_res(filename):
-    """"This function prints the resolution of the jpeg image file passed into it"""
+def transpose(text):
+    if not text:
+        return ""
 
-    # open image for reading in binary mode
-    with open(filename, 'rb') as img_file:
-        # height of image (in 2 bytes) is at 164th position
-        img_file.seek(163)
+    rows = text.split('\n')
 
-        # read the 2 bytes
-        a = img_file.read(2)
+    max_len = 0
+    for row in rows:
+        if len(row) > max_len:
+            max_len = len(row)
 
-        # calculate height
-        height = (a[0] << 8) + a[1]
+    padded_rows = []
+    for row in rows:
+        padded_rows.append(row.ljust(max_len))
 
-        # next 2 bytes is width
-        a = img_file.read(2)
+    transposed = []
+    for col in range(max_len):
+        new_row = ""
+        for row in padded_rows:
+            new_row += row[col]
+        transposed.append(new_row)
 
-        # calculate width
-        width = (a[0] << 8) + a[1]
-
-    print("The resolution of the image is", width, "x", height)
-
-
-jpeg_res("img1.jpg")
+    return '\n'.join(transposed).rstrip()
