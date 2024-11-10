247b9d8e277f5942dd7c08f78f6e412147ed5fe5
foxxxxxxx7 <rob.fox7@hotmail.com>
2024-11-10 23:25:21 +0000

    Optimize Sublist

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def sum_of_multiples(limit, multiples):
-    result = set()
-    for m in multiples:
-        if m != 0:
-            for n in range(m, limit, m):  # Start from m, step by m
-                result.add(n)
-    return sum(result)
+# Possible sublist categories
+EQUAL = 1
+UNEQUAL = 2
+SUBLIST = 3
+SUPERLIST = 4
+
+
+def sublist(list_one, list_two):
+    len_one, len_two = len(list_one), len(list_two)
+
+    if list_one == list_two:
+        return EQUAL
+    elif len_one < len_two:
+        for i in range(len_two - len_one + 1):
+            if list_two[i:i + len_one] == list_one:
+                return SUBLIST
+    elif len_one > len_two:
+        for i in range(len_one - len_two + 1):
+            if list_one[i:i + len_two] == list_two:
+                return SUPERLIST
+
+    return UNEQUAL
